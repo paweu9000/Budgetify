ed672e34cfaab3024078dadbc5edcf9455a8cf8d
paweu9000 <wolfenorrin5@gmail.com>
2022-10-09 19:03:55 +0200

    refactored historyService and added historyController

--- backend/src/main/java/com/example/demo/history/service/HistoryService.java
+++ backend/src/main/java/com/example/demo/history/service/HistoryService.java
@@ -1,7 +1,11 @@
 package com.example.demo.history.service;
 
+import com.example.demo.history.model.History;
 import com.example.demo.history.repository.HistoryRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class HistoryService {
@@ -9,5 +13,18 @@ public class HistoryService {
 
     public HistoryService(HistoryRepository repository) {
         this.repository = repository;
+    }
+
+    public History findById(UUID id) {
+        return unwrapHistory(id);
+    }
+
+    public History unwrapHistory(UUID id) {
+        Optional<History> history = this.repository.findById(id);
+        if (history.isPresent()) {
+            return history.get();
+        } else {
+            throw new RuntimeException("History Object with id: " + id.toString() + " does not exist!");
+        }
     }
 }
