9e788709e0d29499ef5eef8dad4d65571aaa5041
paweu9000 <wolfenorrin5@gmail.com>
2022-10-10 19:57:09 +0200

    added spendings webcontroller and refactored spendings service

--- backend/src/main/java/com/example/demo/spendings/service/SpendingsService.java
+++ backend/src/main/java/com/example/demo/spendings/service/SpendingsService.java
@@ -1,7 +1,11 @@
 package com.example.demo.spendings.service;
 
+import com.example.demo.spendings.model.Spendings;
 import com.example.demo.spendings.repository.SpendingsRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class SpendingsService{
@@ -10,5 +14,17 @@ public class SpendingsService{
 
     public SpendingsService(SpendingsRepository repository) {
         this.repository = repository;
+    }
+
+    public Spendings findById(UUID id) {
+        return unwrapSpendings(id);
+    }
+    public Spendings unwrapSpendings(UUID id) {
+        Optional<Spendings> spendings = repository.findById(id);
+        if (spendings.isPresent()) {
+            return spendings.get();
+        } else {
+            throw new RuntimeException("Savings object with id: " + id.toString() + " does not exist!");
+        }
     }
 }
