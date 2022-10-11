87766777ca20e5e52336d90c1ba472d34d68bcd2
paweu9000 <wolfenorrin5@gmail.com>
2022-10-11 18:45:04 +0200

    created user repository and service

---
+++ backend/src/main/java/com/example/demo/user/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.example.demo.user.repository;
+
+import com.example.demo.user.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsernameOrEmail(String username, String email);
+    Optional<User> findByUsername(String username);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
+}
