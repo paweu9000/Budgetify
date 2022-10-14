03c5cbe2bbec470cbce26dbe7fec2216078ec4a1
paweu9000 <wolfenorrin5@gmail.com>
2022-10-14 19:21:59 +0200

    refactored role and created CustomUserDetailsService

--- backend/src/main/java/com/example/demo/user/roles/Role.java
+++ backend/src/main/java/com/example/demo/user/roles/Role.java
@@ -13,4 +13,16 @@ public class Role {
 
     @Column(length = 60)
     private String name;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
