3f0aa9e3f0454ac3062e326885422931aca16069
paweu9000 <wolfenorrin5@gmail.com>
2022-10-16 21:41:45 +0200

    setup getters for logindto and added login endpoint to auth controller

--- backend/src/main/java/com/example/demo/dto/LoginDto.java
+++ backend/src/main/java/com/example/demo/dto/LoginDto.java
@@ -3,4 +3,12 @@ package com.example.demo.dto;
 public class LoginDto {
     private String usernameOrEmail;
     private String password;
+
+    public String getUsernameOrEmail() {
+        return usernameOrEmail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
--- backend/src/main/java/com/example/demo/user/webcontroller/AuthController.java
+++ backend/src/main/java/com/example/demo/user/webcontroller/AuthController.java
@@ -1,10 +1,17 @@
 package com.example.demo.user.webcontroller;
 
+import com.example.demo.dto.LoginDto;
 import com.example.demo.user.repository.UserRepository;
 import com.example.demo.user.roles.repository.RoleRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +32,11 @@ public class AuthController {
         this.passwordEncoder = passwordEncoder;
     }
 
-
+    @PostMapping("/signin")
+    public ResponseEntity<String> authenticateUser(@RequestBody LoginDto loginDto) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                loginDto.getUsernameOrEmail(), loginDto.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return new ResponseEntity<>("User signed in successfully!", HttpStatus.OK);
+    }
 }
