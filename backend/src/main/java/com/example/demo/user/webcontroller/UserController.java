6dccc9be2b97e454654183c41460078cfc227d22
paweu9000 <wolfenorrin5@gmail.com>
2022-10-21 19:33:07 +0200

    created getUsername method

--- backend/src/main/java/com/example/demo/user/webcontroller/UserController.java
+++ backend/src/main/java/com/example/demo/user/webcontroller/UserController.java
@@ -1,6 +1,10 @@
 package com.example.demo.user.webcontroller;
 
 import com.example.demo.user.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +16,11 @@ public class UserController {
 
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @GetMapping("/name")
+    public ResponseEntity<String> getUsername(@CurrentSecurityContext(expression = "authentication?.name")
+                                                  String username) {
+        return new ResponseEntity<>(username, HttpStatus.OK);
     }
 }
