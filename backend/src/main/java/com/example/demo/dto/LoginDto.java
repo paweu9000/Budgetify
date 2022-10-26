7c91dc208b69bf6151a066dd3c6d4d6a9488e19d
paweu9000 <wolfenorrin5@gmail.com>
2022-10-26 18:45:38 +0200

    refactored logindto for validation

--- backend/src/main/java/com/example/demo/dto/LoginDto.java
+++ backend/src/main/java/com/example/demo/dto/LoginDto.java
@@ -1,7 +1,12 @@
 package com.example.demo.dto;
 
+import javax.validation.constraints.NotEmpty;
+
 public class LoginDto {
+    @NotEmpty
     private String usernameOrEmail;
+
+    @NotEmpty
     private String password;
 
     public String getUsernameOrEmail() {
