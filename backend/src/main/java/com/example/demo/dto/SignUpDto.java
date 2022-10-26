ede34220bf98ed28b1169a8852627f45efa4ae4b
paweu9000 <wolfenorrin5@gmail.com>
2022-10-26 18:18:45 +0200

    refactored SingUpDto for validation

--- backend/src/main/java/com/example/demo/dto/SignUpDto.java
+++ backend/src/main/java/com/example/demo/dto/SignUpDto.java
@@ -1,9 +1,25 @@
 package com.example.demo.dto;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 public class SignUpDto {
+
+    @NotEmpty
+    @Size(min = 5, message = "Login should have at least 5 characters!")
     private String login;
+
+    @NotEmpty
+    @Size(min = 4, message = "Username should have at least 4 characters!")
     private String username;
+
+    @NotEmpty
+    @Email
     private String email;
+
+    @NotEmpty
+    @Size(min = 8, message = "Password should have at least 8 characters!")
     private String password;
 
     public String getLogin() {
