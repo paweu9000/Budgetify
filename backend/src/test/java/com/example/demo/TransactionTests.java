e0b6b7fac592782dcd3a28c379debfc35e2c2a06
pawel <wolfenorrin5@gmail.com>
2022-10-31 15:34:16 +0100

    created validTransaction POST test

--- backend/src/test/java/com/example/demo/TransactionTests.java
+++ backend/src/test/java/com/example/demo/TransactionTests.java
@@ -1,10 +1,39 @@
 package com.example.demo;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class TransactionTests {
 
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    @WithUserDetails("test@email.com")
+    public void validTransactionPostTest() throws Exception {
+        MockHttpServletRequestBuilder mockRequest = post("/api/transaction")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content("{\"description\": \"test_description\", \"budgetType\": \"SAVINGS\"," +
+                        "\"amount\": \"200.0\"}");
+        mvc.perform(mockRequest).andExpect(status().isOk());
+    }
 }
