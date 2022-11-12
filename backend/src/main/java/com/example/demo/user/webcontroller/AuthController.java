04c8ab6063031868f9c81775109e17fc04b549e1
paweu9000 <wolfenorrin5@gmail.com>
2022-11-12 20:46:59 +0100

    created isAuthenticated endpoint

--- backend/src/main/java/com/example/demo/user/webcontroller/AuthController.java
+++ backend/src/main/java/com/example/demo/user/webcontroller/AuthController.java
@@ -8,15 +8,13 @@ import com.example.demo.user.roles.Role;
 import com.example.demo.user.roles.repository.RoleRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.Collections;
@@ -68,5 +66,12 @@ public class AuthController {
         userRepository.save(user);
 
         return new ResponseEntity<>("User registered successfully", HttpStatus.OK);
+    }
+
+    @GetMapping("/authenticated")
+    public ResponseEntity<?> isAuthenticated() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication instanceof AnonymousAuthenticationToken) return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
